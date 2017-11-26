db5547ba3329a981d89f3b81299f7d8b87488977
Pitu <7425261+Pitu@users.noreply.github.com>
2017-11-25 21:01:07 -0300

    Added config and frontend changes

---
+++ src/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	server: {
+		port: 5001,
+		rateLimits: {
+			window: 5,
+			max: 2
+		},
+		secret: 'ZZLgqgXXGXTfj#=fjdj-*nYg3V@PN6_w%&-8xPCF%7LVxc8Ay7Jm^e@Rs%U4#AQD',
+		routePrefix: '',
+		baseURL: 'https://magane.moe/packs/'
+	},
+	pictures: { height: 180 }
+};
