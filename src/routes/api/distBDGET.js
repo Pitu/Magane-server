234d040474100cf5d0da39e5eb375ea7401dc041
Pitu <7425261+Pitu@users.noreply.github.com>
2017-11-26 22:33:46 -0300

    fixed betterdiscord link

--- src/routes/api/distBDGET.js
+++ src/routes/api/distBDGET.js
@@ -11,18 +11,8 @@ class PackGET extends Route {
 	}
 
 	run(req, res) {
-		const content = oneLine`var magane = function () {};
-		magane.prototype.start = function () {
-			document.head.appendChild(document.createElement('script')).setAttribute('src', 'https://magane.moe/api/dist/magane');
-		};
-		magane.prototype.load = function () {};
-		magane.prototype.unload = function () {};
-		magane.prototype.stop = function () {};
-		magane.prototype.getSettingsPanel = function () {};
-		magane.prototype.getName = function () { return 'Bringing LINE stickers to Discord in a chaotic way.'; };
-		magane.prototype.getDescription = function () { return 'Magane'; };
-		magane.prototype.getVersion = function () { return '0.1.0'; };
-		magane.prototype.getAuthor = function () { return 'Kana'; };`;
+		const content = `//META{"name":"magane"}*//
+var magane = function () {}; magane.prototype.start = function () { document.head.appendChild(document.createElement('script')).setAttribute('src', 'https://magane.moe/api/dist/magane'); }; magane.prototype.load = function () {}; magane.prototype.unload = function () {}; magane.prototype.stop = function () {}; magane.prototype.getSettingsPanel = function () {}; magane.prototype.getName = function () { return 'Magane'; }; magane.prototype.getDescription = function () { return 'Bringing LINE stickers to Discord in a chaotic way.'; }; magane.prototype.getVersion = function () { return '0.1.0'; }; magane.prototype.getAuthor = function () { return 'Kana'; };`;
 
 		res.setHeader('Content-type', 'application/octet-stream');
 		res.setHeader('Content-disposition', 'attachment; filename=magane.plugin.js');
