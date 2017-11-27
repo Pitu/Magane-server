97c3f9d25524a65c50ead14b6edee7d1de815c1e
Pitu <7425261+Pitu@users.noreply.github.com>
2017-11-26 21:30:31 -0300

    Script endpoints

---
+++ src/routes/api/distBDGET.js
@@ -0,0 +1,33 @@
+const Route = require('../../structures/Route');
+const { oneLine } = require('common-tags');
+
+class PackGET extends Route {
+	constructor() {
+		super('/api/dist/betterdiscord', 'get');
+	}
+
+	authorize(req, res) {
+		return this.run(req, res);
+	}
+
+	run(req, res) {
+		const content = oneLine`var magane = function () {};
+		magane.prototype.start = function () {
+			document.head.appendChild(document.createElement('script')).setAttribute('src', 'https://magane.moe/api/dist/magane');
+		};
+		magane.prototype.load = function () {};
+		magane.prototype.unload = function () {};
+		magane.prototype.stop = function () {};
+		magane.prototype.getSettingsPanel = function () {};
+		magane.prototype.getName = function () { return 'Bringing LINE stickers to Discord in a chaotic way.'; };
+		magane.prototype.getDescription = function () { return 'Magane'; };
+		magane.prototype.getVersion = function () { return '0.1.0'; };
+		magane.prototype.getAuthor = function () { return 'Kana'; };`;
+
+		res.setHeader('Content-type', 'application/octet-stream');
+		res.setHeader('Content-disposition', 'attachment; filename=magane.plugin.js');
+		res.send(content);
+	}
+}
+
+module.exports = PackGET;
