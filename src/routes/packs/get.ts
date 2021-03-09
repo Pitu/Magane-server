5f5229b70579ab4c89a71a11ee3695aa938cd957
Pitu <heyitspitu@gmail.com>
2021-03-10 01:36:14 +0900

    fix: object returned by api

--- src/routes/packs/get.ts
+++ src/routes/packs/get.ts
@@ -2,19 +2,30 @@ import { Request, Response } from 'express';
 import prisma from '../../structures/database';
 
 export const run = async (req: Request, res: Response): Promise<any> => {
-	const packs = await prisma.packs.findMany({ select: { name: true, count: true, lineId: true, animated: true } });
-	for (const pack of packs) {
-		// @ts-ignore
-		pack.files = [];
-		const stickers = await prisma.stickers.findMany({ where: { packId: pack.lineId }, select: { file: true, lineId: true } });
-		for (const sticker of stickers) {
-			// @ts-ignore
-			pack.files.push(sticker.file);
+	const packs = await prisma.packs.findMany({
+		select: {
+			name: true,
+			count: true,
+			lineId: true,
+			animated: true,
+			stickers: {
+				select: {
+					file: true
+				}
+			}
 		}
-	}
+	});
+
+	const responsePacks = packs.map(pack => ({
+		id: pack.lineId,
+		name: pack.name,
+		count: pack.count,
+		animated: pack.animated,
+		files: pack.stickers.map(sticker => sticker.file)
+	}));
 
 	return res.status(200).json({
 		baseURL: process.env.baseURL,
-		packs
+		packs: responsePacks
 	});
 };
