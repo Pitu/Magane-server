a0663bde0a078091cefdc6cf2a86fcd0353fc4ac
Pitu <heyitspitu@gmail.com>
2021-03-10 02:08:15 +0900

    feat: subscribe count route

---
+++ src/routes/packs/subscribe/post.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import prisma from '../../../structures/database';
+
+export const run = (req: Request, res: Response) => {
+	if (!req.body) return res.status(400).json({ message: 'No body provided' });
+	const { packs }: { packs: number[] } = req.body;
+	if (!packs.length) return res.status(400);
+
+	packs.forEach(pack => {
+		void prisma.packs.update({ where: { lineId: pack }, data: { subscribed: { increment: 1 } } });
+	});
+
+	return res.status(200);
+};
